f2003765f946899681ae87394c9fabbb9fe2d586
yashsinha501 <72601810+yashsinha501@users.noreply.github.com>
2023-10-02 06:06:19 +0530

    setup basic bosyParser

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-const express =  require("express")
-const {PORT} = require('./config/serverConfig')
+const express =  require("express");
+const bodyParser = require('body-parser')
+
+const {PORT} = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
 
     // create a express object
     const app = express();
+
+
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended:true}))
+
+
     app.listen(PORT,()=>{
         console.log(`server started at ${PORT}`);
-    })
-
+    });
 }
 
 setupAndStartServer();
